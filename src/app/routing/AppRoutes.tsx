e9add8fd156bd7e4840d32dd341b2d6614cc9301
nguyentoan06 <toan@myspa.vn>
2023-06-21 20:56:50 +0700

    [REBUILD] 3

--- src/app/routing/AppRoutes.tsx
+++ src/app/routing/AppRoutes.tsx
@@ -5,29 +5,35 @@
  * components (e.g: `src/app/modules/Auth/pages/AuthPage`, `src/app/BasePage`).
  */
 
-import {FC} from 'react'
-import {Routes, Route, BrowserRouter, Navigate} from 'react-router-dom'
-import {PrivateRoutes} from './PrivateRoutes'
-import {ErrorsPage} from '../modules/errors/ErrorsPage'
-import {Logout, AuthPage, useAuth} from '../modules/auth'
-import {App} from '../App'
+import { FC } from 'react'
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
+import { PrivateRoutes } from './PrivateRoutes'
+import { ErrorsPage } from '../modules/errors/ErrorsPage'
+import { Logout, AuthPage, useAuth } from '../modules/auth'
+import About from '@/app/pages/about'
+import Contact from '@/app/pages/contact'
+import { App } from '../App'
+import Home from '@/app/pages/home'
 
 /**
  * Base URL of the website.
  *
  * @see https://facebook.github.io/create-react-app/docs/using-the-public-folder
  */
-const {PUBLIC_URL} = import.meta.env
+const { PUBLIC_URL } = import.meta.env
 
 const AppRoutes: FC = () => {
-  const {currentUser} = useAuth()
+  const { currentUser } = useAuth()
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
-          <Route path='error/*' element={<ErrorsPage />} />
+          {/* <Route path='error/*' element={<ErrorsPage />} /> */}
           <Route path='logout' element={<Logout />} />
-          {currentUser ? (
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='/*' element={<Home />} />
+          {/* {currentUser ? (
             <>
               <Route path='/*' element={<PrivateRoutes />} />
               <Route index element={<Navigate to='/dashboard' />} />
@@ -37,11 +43,11 @@ const AppRoutes: FC = () => {
               <Route path='auth/*' element={<AuthPage />} />
               <Route path='*' element={<Navigate to='/auth' />} />
             </>
-          )}
+          )} */}
         </Route>
       </Routes>
     </BrowserRouter>
   )
 }
 
-export {AppRoutes}
+export { AppRoutes }
