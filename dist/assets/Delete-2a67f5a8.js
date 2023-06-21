86f87effcabb58cc92d9fe60db0c62be3f7432a3
nguyentoan06 <toan@myspa.vn>
2023-06-21 20:39:25 +0700

    [REBUILD]

--- dist/assets/Delete-2a67f5a8.js
+++
@@ -1,1 +0,0 @@
-import{r,i as a,a as t}from"./jsx-runtime_commonjs-proxy-4ae2505d.js";var e={},u=a;Object.defineProperty(e,"__esModule",{value:!0});var l=e.default=void 0,v=u(r()),i=t,o=(0,v.default)((0,i.jsx)("path",{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"}),"Delete");l=e.default=o;export{l as d};
